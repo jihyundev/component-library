9bace2f2b545247bbe83aa7e564e31ec867756f1
jihyundev <aawlgus12@gmail.com>
2024-01-16 23:43:47 +0900

    fix: prevent firing children's click event while finishing drag

--- src/components/Scroller/DraggableScroller.tsx
+++ src/components/Scroller/DraggableScroller.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, MouseEvent, CSSProperties, useRef, useState } from 'react';
+import { ReactNode, MouseEvent, CSSProperties, useRef, useState, useMemo } from 'react';
 import styled from '@emotion/styled';
 
 type Props = {
@@ -18,31 +18,59 @@ const Container = styled.div<DivStyleProps>`
 `;
 
 export const DraggableScroller = ({ children, maxWidth, style }: Props) => {
-  const containerRef = useRef<HTMLDivElement | null>(null);
+  const containerRef = useRef<HTMLDivElement | null>();
 
   const [isDragging, setIsDragging] = useState<boolean>(false);
-  const [startX, setStartX] = useState<number>(0);
+  const [startX, setStartX] = useState<number>(0)
+  const [totalX, setTotalX] = useState<number>(0);
+
+  // addEventListener, removeEventListener 가 동일한 함수 인스턴스를 참조하도록 메모이제이션 추가
+  const preventClick = useMemo(() => (e: MouseEvent) => {
+    e.preventDefault()
+    e.stopPropagation()
+  }, [])
 
   const onDragStart = (e: MouseEvent) => {
     setIsDragging(true);
-    if (containerRef.current) {
-      setStartX(e.pageX + containerRef.current.scrollLeft);
+    const x = e.clientX
+    setStartX(x)
+    if (containerRef.current && 'scrollLeft' in containerRef.current) {
+      setTotalX(x + containerRef.current.scrollLeft);
     }
   };
 
-  const onDragEnd = () => {
+  const onDragEnd = (e: MouseEvent) => {
+    if (!isDragging) return;
+
+    preventClick(e)
     setIsDragging(false);
+
+    const endX = e.clientX;
+    const childNodes = [...(containerRef.current?.childNodes || [])]
+
+    if (startX !== endX) {
+      childNodes.forEach((child) => {
+        child.addEventListener("click", preventClick);
+      })
+    } else {
+      childNodes.forEach((child) => {
+        child.removeEventListener("click", preventClick);
+      })
+    }
   };
 
   const onDragMove = (e: MouseEvent) => {
     if (!isDragging) return;
 
-    // 이동해야 할 x축 거리
-    const deltaX = startX - e.pageX;
+    // 클릭 등 마우스 이동 외 다른 이벤트 실행되는 것 방지
+    preventClick(e)
 
-    if (containerRef.current) {
+    // 스크롤 포지션
+    const scrollLeft = totalX - e.clientX;
+
+    if (containerRef.current && 'scrollLeft' in containerRef.current) {
       // 스크롤 발생
-      containerRef.current.scrollLeft = deltaX;
+      containerRef.current.scrollLeft = scrollLeft;
     }
   };
 
